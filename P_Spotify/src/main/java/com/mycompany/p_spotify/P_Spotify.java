62dcf642c996feef14153d801381522eaab0d8d3
javierlp04 <javierlopez04@umes.edu.gt>
2024-10-09 22:28:18 -0600

    Primer commit

---
+++ P_Spotify/src/main/java/com/mycompany/p_spotify/P_Spotify.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p_spotify;
+
+/**
+ *
+ * @author javib
+ */
+public class P_Spotify {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
