12c8231aac611ddde6a72ac37321c0604b4b0c85
javierlp04 <javierlopez04@umes.edu.gt>
2024-10-09 23:50:55 -0600

    form y clase

---
+++ P_Spotify/src/main/java/logica/canciones.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+/**
+ *
+ * @author javib
+ */
+public class canciones {
+    private int noCancion;
+
+    public canciones() {
+    }
+
+    public canciones(int noCancion) {
+        this.noCancion = noCancion;
+    }
+    
+    
+}
